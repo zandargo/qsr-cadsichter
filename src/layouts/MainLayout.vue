d958e00ee4e447cd05fc7fda4d1440cb0e32b514
Madson Germano <madson.br@gmail.com>
2021-08-23 09:37:16 -0300

    WIP

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="main_div">
+	<div id="main_div" class="no-scroll">
 		<!--
 	Anything after view="lHh lpr lFf" is only needed
 	so we can display this example in the documentation
@@ -9,49 +9,13 @@
 
 		<q-layout view="lHh lpr lFf">
 			<q-header elevated>
-				<TitleBar id="title-bar" elevated />
-				<Ribbon class="text-black" />
+				<TitleBar id="row-title" elevated class="shadow-3" />
+				<Ribbon id="row-ribbon" class="text-black" />
 			</q-header>
 
 			<q-page-container class="q-ma-none q-pa-none">
 				<!-- <router-view /> -->
 				<!-- <Home /> -->
-
-				<div class="text-black">
-					<q-splitter
-						class="text-black"
-						v-model="splitterModel"
-						:limits="[300, 600]"
-						unit="px"
-						:separator-style="{ cursor: 'col-resize' }"
-					>
-						<template v-slot:before>
-							<div class="q-pa-md">
-								<div class="text-h4 q-mb-md">Before</div>
-								<div v-for="n in 5" :key="n" class="q-my-md">
-									{{ n }}. Lorem ipsum dolor sit, amet consectetur
-									adipisicing elit. Quis praesentium cumque magnam odio
-									iure quidem, quod illum numquam possimus obcaecati
-									commodi minima assumenda consectetur culpa fuga nulla
-									ullam. In, libero.
-								</div>
-							</div>
-						</template>
-
-						<template v-slot:after>
-							<div class="q-pa-md">
-								<div class="text-h4 q-mb-md">After</div>
-								<div v-for="n in 20" :key="n" class="q-my-md">
-									{{ n }}. Lorem ipsum dolor sit, amet consectetur
-									adipisicing elit. Quis praesentium cumque magnam odio
-									iure quidem, quod illum numquam possimus obcaecati
-									commodi minima assumenda consectetur culpa fuga nulla
-									ullam. In, libero.
-								</div>
-							</div>
-						</template>
-					</q-splitter>
-				</div>
 			</q-page-container>
 		</q-layout>
 	</div>
@@ -92,8 +56,12 @@ export default defineComponent({
 #main_div {
 	overflow: hidden;
 }
-#title-bar {
-	box-shadow: 5px 5px blue, 10px 10px red, 15px 15px green !important;
-	z-index: 999;
+#row-title {
+	// box-shadow: 0px 5px blue !important;
+	z-index: 9999;
+}
+
+#row-ribbon {
+	z-index: 9000;
 }
 </style>
--- src/pages/Ribbon.vue
+++ src/pages/Ribbon.vue
@@ -1,17 +1,23 @@
 <template>
 	<div id="ribbon" class="column justify-end">
-		<div class="col-auto"></div>
+		<!-- <div class="col-auto"></div> -->
 		<!-- <div id="row_tabs"></div> -->
 		<!-- <div id="row_buttons"></div> -->
 
 		<div class="q-gutter-y-none">
-			<q-tabs v-model="tab" dense align="left" id="row_tabs">
+			<q-tabs
+				v-model="tab"
+				dense
+				align="left"
+				id="row_tabs"
+				class="bg-cs-l4"
+			>
 				<q-tab name="flow" label="Esquema" />
 				<q-tab name="chart" label="Esquemino" />
 				<q-tab name="defs" label="Definições" />
 			</q-tabs>
 
-			<q-tab-panels v-model="tab" animated id="row_buttons">
+			<q-tab-panels v-model="tab" animated id="row_buttons" class="bg-cs-l3">
 				<q-tab-panel name="flow"> Botões de Esquema </q-tab-panel>
 
 				<q-tab-panel name="chart"> Botões de Esquemino </q-tab-panel>
@@ -37,18 +43,19 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 #ribbon {
-	height: $h_title + 8px + $h_tabs + $h_ribbon;
+	height: $h_title + 28px + $h_tabs + $h_ribbon;
 	font-variant-caps: petite-caps !important;
-	// background-color: $color-d3;
-	background-color: $color-l4;
+	// z-index: 100;
 
 	#row_tabs {
 		height: $h_tabs;
+		// background-color: $color-l4;
+		// z-index: 90;
 	}
 
 	#row_buttons {
 		height: $h_ribbon;
-		background-color: $color-l3;
+		// z-index: 1000;
 	}
 }
 
