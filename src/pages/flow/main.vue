a78bf8a84df55b99a7a2bf2f6f485f0557beb831
Madson Germano <madson.br@gmail.com>
2021-09-29 12:40:51 -0300

    WIP

--- src/pages/flow/main.vue
+++ src/pages/flow/main.vue
@@ -1,5 +1,35 @@
 <template>
-	<q-page> Flow main </q-page>
+	<q-page class="row">
+		<!-- //* ---------------------------- DIV CÓDIGOS GPF --------------------------- *// -->
+		<div
+			class="
+				col-5 col-md-4 col-lg-3 col-xl-2
+				q-pa-md
+				items-center
+				text-center
+			"
+		></div>
+
+		<!-- //* ------------------------ DIV DESENHO DAS GAVETAS ----------------------- *// -->
+		<div
+			class="
+				col-6 col-md-5 col-lg-4 col-xl-3
+				q-pa-md
+				items-center
+				text-center
+			"
+		></div>
+
+		<!-- //* ------------------------ DIV SLIDES DAS ALTURAS ------------------------ *// -->
+		<div
+			class="
+				col-1 col-md-3 col-lg-3 col-xl-2
+				q-pa-md
+				items-center
+				text-center
+			"
+		></div>
+	</q-page>
 </template>
 
 <script>
