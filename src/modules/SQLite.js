68c890a09b9c9e2372840942e05a6f36e2959392
Madson Germano <madson.br@gmail.com>
2022-02-10 14:47:44 -0300

    WIP

--- src/modules/SQLite.js
+++ src/modules/SQLite.js
@@ -1,13 +1,19 @@
-import sqlite3 from "sqlite3";
-import knex from "knex";
+// import sqlite3 from "sqlite3";
+// import knex from "knex";
 
-const db = require("knex")({
-	client: "sqlite3",
-	connection: {
-		filename: "public/data/SB_FTP_PLANSICHTER.db",
-	},
-});
+// export const db = require("knex")({
+// 	client: "sqlite3",
+// 	connection: {
+// 		filename: "public/data/SB_FTP_PLANSICHTER.db",
+// 	},
+// });
 
-export function testSQLite(id) {
-	return db("Reg_SQMA").where({ Key: id });
-}
+// export async function testSQLite(id) {
+// 	return await db("Reg_SQMA").where({ Key: id });
+// }
+
+export const sqlite3 = require('sqlite3').verbose()
+export const db = new sqlite3.Database('public/data/SB_FTP_PLANSICHTER.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) { console.error(err.message) }
+  console.log('Connected to the Plansichter database.')
+})
--- src/pages/defs/main.vue
+++ src/pages/defs/main.vue
@@ -1,18 +1,27 @@
 <template>
-	<q-page> Defs main {{ teste }} </q-page>
+	<q-page> Defs main  </q-page>
 </template>
 
 <script>
 // import { reactive, toRefs } from "vue";
 // import { testSQLite } from "src/modules/SQLite";
+
+// import { db } from "src/modules/SQLite";
 import { onMounted } from "vue";
 
 export default {
 	name: "DefsMain",
 
 	setup() {
+
+
 		onMounted(() => {
-			// const teste = testSQLite(20111);
+			// let sql = `SELECT NoSQMN, Rev FROM Reg_SQMN`
+			// db.each(sql, [], (err, row) => {
+			// 	if (err) {throw err}
+			// 	console.log(`Esquemino Nº ${row.NoSQMN}-${row.Rev}`);
+			// })
+			// const teste = async () => await testSQLite(20111);
 		});
 		return {
 			// teste
